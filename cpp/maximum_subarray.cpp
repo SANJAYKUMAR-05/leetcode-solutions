66e247053678127ebbaea33e25c4e55a4a34adc8
SANJAYKUMAR-05 <150768063+SANJAYKUMAR-05@users.noreply.github.com>
2026-08-23 23:27:50 +0530

    LeetCode: update cpp/maximum_subarray.cpp

---
+++ cpp/maximum_subarray.cpp
@@ -0,0 +1,26 @@
+// ======================================
+// LeetCode Problem: maximum subarray
+// Language: cpp
+// Link: https://leetcode.com/problems/maximum-subarray/
+// Synced by: LinkCode
+// Date: 8/23/2026, 11:27:49 PM
+// ======================================
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0,ans = INT_MIN;
+        for(int num : nums)
+        {
+            sum += num;
+            ans = max(ans,sum);
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return ans;
+    }
+};
