c101e086a6fbdbd978c72bb7c66997f3a01db890
SANJAYKUMAR-05 <150768063+SANJAYKUMAR-05@users.noreply.github.com>
2026-09-01 16:38:40 +0530

    LeetCode: update cpp/kth_smallest_element_in_a_bst.cpp

---
+++ cpp/kth_smallest_element_in_a_bst.cpp
@@ -0,0 +1,43 @@
+// ======================================
+// LeetCode Problem: kth smallest element in a bst
+// Language: cpp
+// Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// Synced by: LinkCode
+// Date: 9/1/2026, 4:38:39 PM
+// ======================================
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = -1;
+    void func(TreeNode* root,int k,int &count)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        func(root->left,k,count);
+        count++;
+        if(k == count)
+        {
+            ans = root->val;
+        }
+        func(root->right,k,count);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int count = 0;
+       func(root,k,count);
+       return ans;
+    }
+};
