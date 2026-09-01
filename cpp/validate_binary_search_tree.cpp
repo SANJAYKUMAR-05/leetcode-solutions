aac232b987296be66f7ba1334b2e4af5df2309b2
SANJAYKUMAR-05 <150768063+SANJAYKUMAR-05@users.noreply.github.com>
2026-09-01 21:14:41 +0530

    LeetCode: update cpp/validate_binary_search_tree.cpp

---
+++ cpp/validate_binary_search_tree.cpp
@@ -0,0 +1,38 @@
+// ======================================
+// LeetCode Problem: validate binary search tree
+// Language: cpp
+// Link: https://leetcode.com/problems/validate-binary-search-tree/
+// Synced by: LinkCode
+// Date: 9/1/2026, 9:14:40 PM
+// ======================================
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool func(TreeNode* root,int st,int end)
+    {
+        if(root == NULL)
+        {
+            return true;
+        }
+        if(root->val <= st || root->val >= end)
+        {
+            return false;
+        }
+        return (func(root->left,st,root->val) && func(root->right,root->val,end));
+    }
+    bool isValidBST(TreeNode* root) {
+        return func(root,INT_MIN,INT_MAX);
+    }
+};
